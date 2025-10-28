a738c47efbd1570a6625eadf95e5016816cc562a
CodeByXC <carlaximenarb@gmail.com>
2025-10-28 17:54:43 -0400

    RenderNew5

--- resources/views/layouts/_partials/layout.blade.php
+++ resources/views/layouts/_partials/layout.blade.php
@@ -21,20 +21,6 @@
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/fabric.js/4.6.0/fabric.min.js"></script>
 </head>
-<!-- 
-<!DOCTYPE html>
-<html lang="es">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    @livewireStyles
-    @livewireScripts
-    <link rel="stylesheet" href="/css/app.css">
-    <title>@yield('title')</title>
-    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/fabric.js/4.6.0/fabric.min.js"></script>
-</head>
--->
 <body>
     @include('layouts._partials.messages')
     @livewire('navigation-menu') 
