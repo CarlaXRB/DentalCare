d0c305146e4615858f6c9cb40fb3175bb1cfaffd
CodeByXC <carlaximenarb@gmail.com>
2025-10-28 17:22:27 -0400

    RenderNew1

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -9,10 +9,7 @@
         <title>{{ config('app.name', 'Dental Care') }}</title>
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
-
-        
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
-        <!--<link rel="stylesheet" href="{{ asset('css/app.css')}}"> -->
+        <link rel="stylesheet" href="{{ asset('css/app.css')}}">
         
         @livewireStyles
         <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
