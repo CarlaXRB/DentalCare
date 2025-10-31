651dfaa3d6869f2a17ad287a643b200f21394e7a
CodeByXC <carlaximenarb@gmail.com>
2025-10-30 20:00:48 -0400

    nuevoRenderyGoogle4

--- routes/web.php
+++ routes/web.php
@@ -88,7 +88,7 @@ Route::middleware(['auth:sanctum', config('jetstream.auth_session'), 'verified']
     Route::get('/payments/index', [PaymentController::class, 'index'])->name('payments.index');
 
     Route::resource('multimedia', MultimediaFileController::class);
-    Route::get('multimedia/search', [MultimediaFileController::class, 'search'])->name('multimedia.search');
+    Route::post('multimedia/search', [MultimediaFileController::class, 'search'])->name('multimedia.search');
     Route::get('/multimedia/image/{studyCode}/{fileName}', [MultimediaFileController::class, 'serveImage'])
         ->where('fileName', '.*')->name('multimedia.image');
 });
