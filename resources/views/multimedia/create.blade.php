a585270a666ea3c7896f335ed3aaac7a670e8f2e
CodeByXC <carlaximenarb@gmail.com>
2025-11-04 11:14:16 -0400

    Para aplicar herramientas1

--- resources/views/multimedia/create.blade.php
+++ resources/views/multimedia/create.blade.php
@@ -51,7 +51,7 @@
         <div class="md:col-span-2">
             <label class="title4 block mb-2">{{ __('Descripción') }}:</label>
             <textarea name="description" rows="3"
-                class="w-full px-4 py-2 border border-gray-300 rounded-xl shadow-sm focus:border-cyan-500 focus:ring focus:ring-cyan-300 focus:ring-opacity-50 transition duration-200 ease-in-out text-gray-700 bg-white"/>
+                class="w-full px-4 py-2 border border-gray-300 rounded-xl shadow-sm focus:border-cyan-500 focus:ring focus:ring-cyan-300 focus:ring-opacity-50 transition duration-200 ease-in-out text-gray-700 bg-white">
                 @error('description') <p class="error mt-1">{{ $message }}</p> @enderror
         </div>
         <div class="flex justify-center mt-6">
