2ed2d1bfa9a55928e2019e3f871e4dc1a5077764
CodeByXC <carlaximenarb@gmail.com>
2025-11-04 13:01:21 -0400

    Para aplicar herramientas5

--- resources/views/budgets/edit.blade.php
+++ resources/views/budgets/edit.blade.php
@@ -37,8 +37,8 @@
             <div>
                 <label class="title4 block mb-2">{{ __('Descripción') }}:</label>
                 <textarea name="description" 
-                    class="w-full px-4 py-2 border border-gray-300 rounded-xl shadow-sm focus:border-cyan-500 focus:ring focus:ring-cyan-300 focus:ring-opacity-50 transition duration-200 ease-in-out text-gray-700 bg-white"/>
-                @error('description') <p class="error mt-1">{{ $message }}</p> @enderror
+                    class="border-black rounded-lg p-3 w-full focus:outline-none focus:ring-2 focus:ring-cyan-500">{{ old('description') }}</textarea>
+            @error('description') <p class="error mt-1">{{ $message }}</p> @enderror
             </div>
 
         </div>
--- resources/views/treatments/create.blade.php
+++ resources/views/treatments/create.blade.php
@@ -65,7 +65,7 @@
                 <label class="title4 block mb-2">{{ __('Tipo de Descuento') }}:</label>
                 <select id="discountType" name="discount_type"
                     class="w-full px-4 py-2 border border-gray-300 rounded-xl shadow-sm focus:border-cyan-500 focus:ring focus:ring-cyan-300 focus:ring-opacity-50 transition duration-200 ease-in-out text-gray-700 bg-white"/>
-                    <option value="fixed">{{ __('Cantidad fija') }}</option>
+            <option value="fixed">{{ __('Cantidad fija') }}</option>
                     <option value="percentage">{{ __('Porcentaje (%)') }}</option>
                 </select>
             </div>
@@ -79,8 +79,8 @@
             <div>
                 <label class="title4 block mb-2">{{ __('Detalles') }}:</label>
                 <textarea name="details" 
-                    class="w-full px-4 py-2 border border-gray-300 rounded-xl shadow-sm focus:border-cyan-500 focus:ring focus:ring-cyan-300 focus:ring-opacity-50 transition duration-200 ease-in-out text-gray-700 bg-white"/>
-                @error('details') <p class="error mt-1">{{ $message }}</p> @enderror
+                    class="border-black rounded-lg p-3 w-full focus:outline-none focus:ring-2 focus:ring-cyan-500">{{ old('description') }}</textarea>
+            @error('details') <p class="error mt-1">{{ $message }}</p> @enderror
             </div>
         {{-- Buttons --}}
         <div class="flex justify-center gap-4 mt-6">
